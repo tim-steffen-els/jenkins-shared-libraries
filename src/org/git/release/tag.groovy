2b9e0934dff3cad9df7b226f5989c0f0b2613888
tim-steffen-els <t.steffen@elsevier.com>
2021-01-04 18:09:47 -0500

    trying

--- src/org/git/release/tag.groovy
+++ src/org/git/release/tag.groovy
@@ -1,21 +1,5 @@
 
 
-def call() {
-    node {
-        //git url: "https://github.com/werne2j/sample-nodejs"
-
-        stage("Install") {
-           // sh "npm install"
-           echo "hello world"
-        }
-
-        stage("Test") {
-            echo "hello world"
-        }
-
-        stage("Deploy") {
-            echo "hello world"
-        }
-
-    }
+def call(Map config=[:]) {
+   echo config.message
 }
