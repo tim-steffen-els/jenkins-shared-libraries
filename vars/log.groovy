2cd70179d4158111e5ac0a9071e1ab815b80a3f8
tim-steffen-els <t.steffen@elsevier.com>
2021-01-05 13:44:33 -0500

    trying for git

--- vars/log.groovy
+++ vars/log.groovy
@@ -1,4 +1,3 @@
-import hudson.FilePath
 
 def info(message) {
     echo "INFO: ${message}"
@@ -6,31 +5,5 @@ def info(message) {
 
 def warning(message) {
     echo "WARNING: ${message}"
-}
-
-def checkout(message){
-	echo "CHECKING OUT: ${message}"
-	sh label: '', script: 'git clone https://github.com/tim-steffen-els/jenkins-shared-libraries.git'
-}
-
-def error(message) {
-    pipeline {
-      agent any
-      stages {
-        stage('Even Stage') {
-          steps {
-            echo "The build number is even"
-          }
-        }
-      }
-    }
-  } 
-
-def checkouttest(message) {
-	echo "ERROR: ${message}"
-	sh label: '', script: 'git clone https://github.com/tim-steffen-els/jenkins-shared-libraries.git'
-	echo "ERROR: ${message}"
-	git branch: 'main', url: "git@github.com:tim-steffen-els/jenkins-shared-libraries.git"
-	echo "ERROR: ${message}"
 }
 
