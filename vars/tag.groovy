aaf2dbeaf5ce963e847a0a1621068e4bdeaa08c1
tim-steffen-els <t.steffen@elsevier.com>
2021-01-05 14:37:26 -0500

    trying for git

--- vars/tag.groovy
+++ vars/tag.groovy
@@ -1,48 +1,55 @@
-def call(repo, branch, version) {
-  //pipeline {
-   // agent any
-    stages {
-        stage("Cleaan Workspace"){
-            steps {
-                cleanWs()
-            }
-        }
-        stage("Set Discription"){
-          steps {
-            buildDescription 'V: $version, B: $branch, R: $repo'
-          }
-        }
-        stage('Git checkout') {
-            steps {
-                //We should not be checking out tags since this process is creating them.
-                git branch: branch, url: repo
-            }
-        }
-      //  stage("Update dependencies") {
-      //      when {
-      //          expression {
-      //              //This is used to differenciate between a frontend and backend app.
-      //              return fileExists('package.json')
-      //          }
-      //      }
-      //      steps{
-      //          //This needs the pipeline utility plugin
-      //          updatePackage(VersionUpdates, 'package.json', VERSION)
-      //      }
-      //  }
-        stage('Git create tag') {
-            steps {
-                //If this step fails, the tag already exist. 
-                sh 'git tag -l'
-                sh 'git tag v${VERSION}'
-            }
-        }
-        stage('Create tag in Github') {
-            steps {
-                //If this step fails, it is likely something to do with credentials 
-                sh 'git push origin v${VERSION}'
-            }
-        }
+def call(Map config) {
+    node {
+        git url: config.url
+        
     }
-//}
 }
+
+//def call(repo, branch, version) {
+//  //pipeline {
+//   // agent any
+//    stages {
+//        stage("Cleaan Workspace"){
+//            steps {
+//                cleanWs()
+//            }
+//        }
+//        stage("Set Discription"){
+//          steps {
+//            buildDescription 'V: $version, B: $branch, R: $repo'
+//          }
+//        }
+//        stage('Git checkout') {
+//            steps {
+//                //We should not be checking out tags since this process is creating them.
+//                git branch: branch, url: repo
+//            }
+//        }
+//      //  stage("Update dependencies") {
+//      //      when {
+//      //          expression {
+//      //              //This is used to differenciate between a frontend and backend app.
+//      //              return fileExists('package.json')
+//      //          }
+//      //      }
+//      //      steps{
+//      //          //This needs the pipeline utility plugin
+//      //          updatePackage(VersionUpdates, 'package.json', VERSION)
+//      //      }
+//      //  }
+//        stage('Git create tag') {
+//            steps {
+//                //If this step fails, the tag already exist. 
+//                sh 'git tag -l'
+//                sh 'git tag v${VERSION}'
+//            }
+//        }
+//        stage('Create tag in Github') {
+//            steps {
+//                //If this step fails, it is likely something to do with credentials 
+//                sh 'git push origin v${VERSION}'
+//            }
+//        }
+//    }
+////}
+//}
