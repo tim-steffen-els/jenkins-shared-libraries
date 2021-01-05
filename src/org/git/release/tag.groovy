250118b0fe0cf7d2af176c7f49154b87542dd915
tim-steffen-els <t.steffen@elsevier.com>
2021-01-05 13:08:38 -0500

    trying

--- src/org/git/release/tag.groovy
+++ src/org/git/release/tag.groovy
@@ -8,6 +8,31 @@ def warning(message) {
    echo "WARNING: ${message}"
 }
 
+def call(int buildNumber) {
+  if (buildNumber % 2 == 0) {
+    pipeline {
+      agent any
+      stages {
+        stage('Even Stage') {
+          steps {
+            echo "The build number is even"
+          }
+        }
+      }
+    }
+  } else {
+    pipeline {
+      agent any
+      stages {
+        stage('Odd Stage') {
+          steps {
+            echo "The build number is odd"
+          }
+        }
+      }
+    }
+  }
+}
 def error(message) {
 	echo "ERROR: ${message}"
 	sh label: '', script: 'git clone https://github.com/tim-steffen-els/jenkins-shared-libraries.git'
