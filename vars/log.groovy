483a263b4590c618f89d5c156d45881e5fecdef1
tim-steffen-els <t.steffen@elsevier.com>
2021-01-05 13:30:58 -0500

    trying for git

--- vars/log.groovy
+++ vars/log.groovy
@@ -5,3 +5,29 @@ def info(message) {
 def warning(message) {
     echo "WARNING: ${message}"
 }
+
+
+def error(int buildNumber) {
+  if (buildNumber % 2 == 0) {
+    pipeline {
+      agent any
+      stages {
+        stage('Even Stage') {
+          steps {
+            echo "The build number is even"
+          }
+        }
+      }
+    }
+  } else {
+    pipeline {
+      agent any
+      stages {
+        stage('Odd Stage') {
+          steps {
+            echo "The build number is odd"
+          }
+        }
+      }
+    }
+  }
