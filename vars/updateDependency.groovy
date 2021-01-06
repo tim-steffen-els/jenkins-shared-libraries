6bb5a2953829a9769589dcf603920687d0367a96
tim-steffen-els <t.steffen@elsevier.com>
2021-01-06 15:01:37 -0500

    trying for git

--- vars/updateDependency.groovy
+++ vars/updateDependency.groovy
@@ -9,33 +9,33 @@
 //
 //
 //}
-def update(Map config) {
-    sh label: 'debugging', script: 'ls -lrt'
-    if (fileExists('package.json')) {
-        log.info "Updating dependency ${config.dependency} to version ${config.version}"
-        updatePackage(config.dependency, config.version, "package.json")
-    } else {
-        log.error "There is no package.json file to update. Skipping operation."
-    }
-
-}
-def updatePackage(String dependency, version, fileName){
-
-    def file = readJSON file: fileName
-    file['dependencies'][dependency] = version
-
-    writeJSON file: fileName, json: file, pretty: 4
-
-}
-
-def updateVersion(Map config){
-
-    def file = readJSON file: config.fileName
-    file['version'] = config.version
-
-    writeJSON file: config.fileName, json: file, pretty: 4
-
-}
+//def update(Map config) {
+//    sh label: 'debugging', script: 'ls -lrt'
+//    if (fileExists('package.json')) {
+//        log.info "Updating dependency ${config.dependency} to version ${config.version}"
+//        updatePackage(config.dependency, config.version, "package.json")
+//    } else {
+//        log.error "There is no package.json file to update. Skipping operation."
+//    }
+//
+//}
+//def updatePackage(String dependency, version, fileName){
+//
+//    def file = readJSON file: fileName
+//    file['dependencies'][dependency] = version
+//
+//    writeJSON file: fileName, json: file, pretty: 4
+//
+//}
+//
+//def updateVersion(Map config){
+//
+//    def file = readJSON file: config.fileName
+//    file['version'] = config.version
+//
+//    writeJSON file: config.fileName, json: file, pretty: 4
+//
+//}
 
 
 //def updatePackage(String versionUpdates, fileName, version){
