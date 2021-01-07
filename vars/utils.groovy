6d593a904bef27bebb871caad6699da8ecebf3fa
tim-steffen-els <t.steffen@elsevier.com>
2021-01-07 14:17:52 -0500

    trying for git

---
+++ vars/utils.groovy
@@ -0,0 +1,11 @@
+def checkIfEmpty(string){
+    boolean isEmpty = true
+    if(string == null){
+        log.info 'String is null'
+        isEmpty = true
+    } else if (string.trim().size() == 0){
+        log.info 'String is empty'
+        isEmpty = true
+    }
+    return isEmpty
+}
